cb76b754c10707744d1855e5e639524f0d5fcfc8
skj <skj.jo0c@gmail.com>
2016-12-14 15:22:29 +0545

    xc

--- db/migrate/20161214092746_create_subjects.rb
+++ db/migrate/20161214092746_create_subjects.rb
@@ -1,7 +1,9 @@
 class CreateSubjects < ActiveRecord::Migration[5.0]
   def change
     create_table :subjects do |t|
-
+	t.string :sub_name
+	t.integer :marks_obt
+	t.string :grade
       t.timestamps
     end
   end
